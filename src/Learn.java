09847f26e4c4e550bad1262a1cf31096f8089023
Joshua Thieme <jomthieme@gmail.com>
2022-04-27 16:18:30 -0600

    Learning Java Syntax and IntelliJ

---
+++ src/Learn.java
@@ -0,0 +1,9 @@
+public class Learn {
+    public static void add(int x, int y){
+        System.out.println(x + y);
+    }
+    public static void main(String[] args){
+        System.out.println("hello");
+        add(5,4);
+    }
+}
