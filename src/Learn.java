4b9f4a8acb64832a597357c429ab3f417717d636
Joshua Thieme <jomthieme@gmail.com>
2022-04-27 22:28:57 -0600

    Adjusted the add() method.

--- src/Learn.java
+++ src/Learn.java
@@ -1,6 +1,6 @@
 public class Learn {
     public static void add(int x, int y){
-        System.out.println(x + y);
+        System.out.println(x + " + " + y + " is " + (x + y));
     }
     public static void main(String[] args){
         System.out.println("hello");
